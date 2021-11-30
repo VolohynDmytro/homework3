863ae76dfa93a9dea58028d4e2983e9b7cc427ca
VolohynDmytro <voloshindima777@gmail.com>
2021-11-30 16:57:38 +0200

    Edited html and js files

---
+++ type.js
@@ -0,0 +1,12 @@
+const firstNum = prompt('Enter first number');
+
+const secondNum = prompt('Enter second number');
+
+
+var num1 = Number.parseInt(firstNum);
+console.log(typeof(num1));
+var num2 = Number.parseInt(secondNum);
+console.log(typeof(num2));
+
+alert (`${firstNum} + ${secondNum} = ${num1+num2}`);
+
