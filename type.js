1f53077baac0fea4c2c7b42d099b9ace356bdfd2
VolohynDmytro <voloshindima777@gmail.com>
2021-12-03 18:49:03 +0200

    Added js file hm4

--- type.js
+++ type.js
@@ -2,11 +2,25 @@ const firstNum = prompt('Enter first number');
 
 const secondNum = prompt('Enter second number');
 
+const operation = prompt('Choose operation')
+
+if (operation !== '+' && operation !== '-' && operation !=='/' && operation !== '*'){ 
+alert('You entered non-opetaion symbol')
+}
 
 var num1 = Number.parseInt(firstNum);
 console.log(typeof(num1));
 var num2 = Number.parseInt(secondNum);
 console.log(typeof(num2));
+var oper = Number.parseInt(operation);
 
-alert (`${firstNum} + ${secondNum} = ${num1+num2}`);
+if (operation === '+'){
+alert (`${firstNum} + ${secondNum} = ${num1+num2}`)
+}else if(operation ==='-'){
+alert(`${firstNum} - ${secondNum} = ${num1-num2}`)
+}else if(operation ==='*'){
+alert(`${firstNum} * ${secondNum} = ${num1*num2}`)
+}else if(operation ==='/'){
+alert(`${firstNum} / ${secondNum} = ${num1/num2}`)
+}
 
